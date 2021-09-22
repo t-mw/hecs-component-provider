e8b43fdb239056cf6049e7928fad565cfd0d9be9
Tobias Mansfield-Williams <contact@tmwhere.com>
2021-09-22 02:22:56 +0200

    Update README

--- src/lib.rs
+++ src/lib.rs
@@ -1,18 +1,19 @@
-//! Easily define behavior for sets of components when using the hecs ECS library.
+//! Easily define behavior for sets of components when using the [hecs](https://github.com/Ralith/hecs/) ECS library.
 //!
-//! ```
+//! ```rust
 //! use hecs_component_provider::{
 //!     default_trait_impl, gen_tuple_query_component_providers,
-//!     ComponentProvider, ComponentProviderMut
+//!     ComponentProvider, ComponentProviderMut, QueryComponentProvider
 //! };
 //!
 //! struct Position(f32, f32);
 //! struct Velocity(f32, f32);
+//! struct Enemy { shot_count: i32 };
 //!
 //! // implement a behavior for all entities that provide the required components
 //! #[default_trait_impl]
-//! trait Update: ComponentProviderMut<Position> + ComponentProvider<Velocity> {
-//!     fn update(&mut self, dt: f32) {
+//! trait ApplyVelocity: ComponentProviderMut<Position> + ComponentProvider<Velocity> {
+//!     fn apply_velocity(&mut self, dt: f32) {
 //!         let &Velocity(vx, vy) = self.get();
 //!         let position: &mut Position = self.get_mut();
 //!         position.0 += vx * dt;
@@ -23,20 +24,55 @@
 //! let mut world = hecs::World::new();
 //! world.spawn((Position(1.0, 2.0), Velocity(0.7, 0.8)));
 //!
-//! // prepare a query that returns entities with the components required for the Update behavior
+//! // prepare a query that returns entities with the components required for the behavior
 //! gen_tuple_query_component_providers!(
-//!     Query,
+//!     Query1,
 //!     (&mut Position, &Velocity)
 //! );
 //!
 //! let dt = 0.1;
-//! for (_, mut entity) in world.query_mut::<Query>() {
-//!     // apply the Update behavior to the entity
-//!     entity.update(dt);
+//! for (_, mut entity) in world.query_mut::<Query1>() {
+//!     // apply the behavior to the entity
+//!     entity.apply_velocity(dt);
 //!
 //!     let position = entity.0;
 //!     assert_eq!(position.0, 1.07);
 //!     assert_eq!(position.1, 2.08);
+//! }
+//!
+//!
+//! // behaviors can depend on one another
+//! #[default_trait_impl]
+//! trait EnemyBehaviors: ApplyVelocity + ComponentProviderMut<Enemy> {
+//!     fn shoot_and_move(&mut self, dt: f32) {
+//!         self.shoot();
+//!         self.apply_velocity(dt);
+//!     }
+//!
+//!     fn shoot(&mut self) {
+//!         let enemy: &mut Enemy = self.get_mut();
+//!         enemy.shot_count += 1;
+//!     }
+//! }
+//!
+//! world.spawn((Enemy { shot_count: 0 }, Position(2.0, 3.0), Velocity(-0.7, -0.8)));
+//!
+//! // queries can be prepared using structs instead of tuples
+//! #[derive(hecs::Query, QueryComponentProvider)]
+//! struct Query2<'a> {
+//!     enemy: &'a mut Enemy,
+//!     position: &'a mut Position,
+//!     velocity: &'a Velocity,
+//! }
+//!
+//! let dt = 0.1;
+//! for (_, mut entity) in world.query_mut::<Query2>() {
+//!     // apply the behavior to the entity
+//!     entity.shoot_and_move(dt);
+//!
+//!     assert_eq!(entity.enemy.shot_count, 1);
+//!     assert_eq!(entity.position.0, 1.93);
+//!     assert_eq!(entity.position.1, 2.92);
 //! }
 //! ```
 
